51b14afa906d886db9c5d34357a1c2eb92b37a93
Attuah Ubong Kingsley <54309563+adunel001@users.noreply.github.com>
2022-07-15 15:27:28 +0000

    Challenge 1, and Challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
+
+let sum = 0;
+
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
+}
     return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+(sumOfNumbers)
 
 /**
  * Challenge - 2
@@ -28,10 +34,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
 
+let countEven = 0;
+for (let i = 0; i < arrayOfNumbers.length; i++);
+  if (arrayOfNumbers [i] % 2 === 0) {
+	countEven++
+  }
+  return countEven;
+}
+(countEvenNumbers)
 /**
  * Challenge - 3
  * 
